ffaaa0f248405fb2b7b6d820f2c5d7adf6ea0cf9
itzenoughabhi <abhishek1432h@gmail.com>
2025-09-28 11:40:27 +0530

    1

--- app/api/inngest/route.js
+++ app/api/inngest/route.js
@@ -1,12 +1,13 @@
 import { serve } from "inngest/next";
-import { inngest, syncUserCreation, syncUserDeletion } from "@/config/inngest";
-
+import { inngest, syncUserCreation, syncUserUpdate, syncUserDeletion } from "@/config/inngest";
 
 export const { GET, POST, PUT } = serve({
   client: inngest,
   functions: [
     syncUserCreation,
-    syncUserDeletion,
+    syncUserUpdate,       // make sure to include all needed functions
     syncUserDeletion
-],
-}); 
+  ],
+});
+import connectDB from "@/config/db";
+connectDB(); // Ensure DB is connected when this module is loaded 
